a704c3833fa20ece68ece5c815f4bb11ba1c8f30
PaulaGarciaMolina <paula.garciamolina@estudiante.uam.es>
2025-07-31 09:21:56 +0200

    Adapt cross_maxvol to optimization

--- src/seemps/analysis/cross/cross_maxvol.py
+++ src/seemps/analysis/cross/cross_maxvol.py
@@ -11,12 +11,15 @@ from .cross import (
     BlackBox,
     maxvol_square,
     _check_convergence,
+    i2s
 )
 from ..sampling import random_mps_indices
 from ...tools import make_logger
 
 # TODO: Implement local error evaluation
 
+def get_max_D(mps):
+    return max([max(t.shape) for t in mps])
 
 @dataclasses.dataclass
 class CrossStrategyMaxvol(CrossStrategy):
@@ -44,73 +47,24 @@ class CrossStrategyMaxvol(CrossStrategy):
     """
 
 
-def cross_maxvol(
-    black_box: BlackBox,
-    cross_strategy: CrossStrategyMaxvol = CrossStrategyMaxvol(),
-    initial_points: np.ndarray | None = None,
-    callback: Callable | None = None,
-) -> CrossResults:
-    """
-    Computes the MPS representation of a black-box function using the tensor cross-approximation (TCI)
-    algorithm based on one-site optimizations using the rectangular maxvol decomposition.
-    The black-box function can represent several different structures. See `black_box` for usage examples.
-
-    Parameters
-    ----------
-    black_box : BlackBox
-        The black box to approximate as a MPS.
-    cross_strategy : CrossStrategy, default=CrossStrategy()
-        A dataclass containing the parameters of the algorithm.
-    initial_points : np.ndarray, optional
-        A collection of initial points used to initialize the algorithm.
-        If None, an initial random point is used.
-    callback : Callable, optional
-        A callable called on the MPS after each iteration.
-        The output of the callback is included in a list 'callback_output' in CrossResults.
-
-    Returns
-    -------
-    CrossResults
-        A dataclass containing the MPS representation of the black-box function,
-        among other useful information.
-    """
-    if initial_points is None:
-        initial_points = random_mps_indices(
-            black_box.physical_dimensions,
-            num_indices=1,
-            allowed_indices=getattr(black_box, "allowed_indices", None),
-            rng=cross_strategy.rng,
-        )
-
-    cross = CrossInterpolationMaxvol(black_box, initial_points)
-    converged = False
-    callback_output = []
-    with make_logger(2) as logger:
-        for i in range(cross_strategy.maxiter):
-            # Forward sweep
-            for k in range(cross.sites):
-                _update_maxvol(cross, k, True, cross_strategy)
-            # Backward sweep
-            for k in reversed(range(cross.sites)):
-                _update_maxvol(cross, k, False, cross_strategy)
-            if callback:
-                callback_output.append(callback(cross.mps, logger=logger))
-            if converged := _check_convergence(cross, i, cross_strategy, logger):
-                break
-        if not converged:
-            logger("Maximum number of iterations reached")
-    points = cross.indices_to_points(False)
-    return CrossResults(
-        mps=cross.mps,
-        points=points,
-        evals=black_box.evals,
-        callback_output=callback_output,
-    )
-
 
 class CrossInterpolationMaxvol(CrossInterpolation):
-    def __init__(self, black_box: BlackBox, initial_point: np.ndarray):
+    def __init__(self, black_box: BlackBox, initial_point: np.ndarray | None = None, cross_strategy: CrossStrategyMaxvol = CrossStrategyMaxvol(),
+        with_cache: bool = False):
+        self.cross_strategy = cross_strategy  # Make sure this is set first
+        if initial_point is None:
+            initial_point = random_mps_indices(
+                black_box.physical_dimensions,
+                num_indices=1,
+                allowed_indices=getattr(black_box, "allowed_indices", None),
+                rng=self.cross_strategy.rng,
+            )
         super().__init__(black_box, initial_point)
+        self.with_cache = with_cache
+        self.cache = {}
+        self.i_opt = None
+        self.y_opt = None
+        self.opt_trajectory = []
 
     @staticmethod
     def combine_indices_fortran(*indices: np.ndarray) -> np.ndarray:
@@ -139,71 +93,262 @@ class CrossInterpolationMaxvol(CrossInterpolation):
 
         return functools.reduce(cartesian_fortran, indices)
 
-
-def _update_maxvol(
-    cross: CrossInterpolationMaxvol,
-    k: int,
-    forward: bool,
-    cross_strategy: CrossStrategyMaxvol,
-) -> None:
-    if cross_strategy.fortran_order is True:
-        combine_indices = cross.combine_indices_fortran
-        order = "F"
-    else:
-        combine_indices = cross.combine_indices
-        order = "C"
-    fiber = cross.sample_fiber(k)
-    r_l, s, r_g = fiber.shape
-    if forward:
-        C = fiber.reshape(r_l * s, r_g, order=order)  # type: ignore
-        Q, _ = scipy.linalg.qr(C, mode="economic", overwrite_a=True, check_finite=False)  # type: ignore
-        I, _ = choose_maxvol(
-            Q,  # type: ignore
-            cross_strategy.rank_kick,
-            cross_strategy.maxiter_maxvol_square,
-            cross_strategy.tol_maxvol_square,
-            cross_strategy.tol_maxvol_rect,
-        )
-        if k < cross.sites - 1:
-            cross.I_l[k + 1] = combine_indices(cross.I_l[k], cross.I_s[k])[I]
-    else:
-        if k > 0:
-            R = fiber.reshape(r_l, s * r_g, order=order)  # type: ignore
-            Q, _ = scipy.linalg.qr(  # type: ignore
-                R.T, mode="economic", overwrite_a=True, check_finite=False
+    def choose_maxvol(self,
+        A: np.ndarray,
+        rank_kick: tuple = (0, np.inf),
+        maxiter: int = 10,
+        tol: float = 1.1,
+        tol_rect: float = 0.1,
+    ) -> tuple[np.ndarray, np.ndarray]:
+        n, r = A.shape
+        max_rank_kick = min(rank_kick[1], n - r)
+        min_rank_kick = min(rank_kick[0], max_rank_kick)
+        if n < r:
+            return np.arange(n, dtype=int), np.eye(n)
+        elif rank_kick == 0:
+            return maxvol_square(A, maxiter, tol)
+        else:
+            return maxvol_rectangular(
+                A, min_rank_kick, max_rank_kick, maxiter, tol, tol_rect
             )
-            I, G = choose_maxvol(
+        
+    def smooth_func(self, y, y0=0.):
+        """Smooth function that transforms max to min."""
+        #return np.pi/2 - np.arctan(y - y0)
+        return np.exp((y0 - y))
+
+    
+    def find_opt(self, I, y, i_opt, y_opt, is_min=True):
+        """Find the minimum or maximum value on a set of sampled points."""
+        y = y.flatten()
+        if is_min:
+            ind = np.argmin(y)
+        else:
+            ind = np.argmax(y)
+        y_opt_curr = y[ind]
+        self.opt_trajectory.append((i2s(I[ind, :]), y_opt_curr, self.black_box.evals, get_max_D(self.mps)))
+        if is_min and y_opt is not None and y_opt_curr >= y_opt:
+            return i_opt, y_opt
+        if not is_min and y_opt is not None and y_opt_curr <= y_opt:
+            return i_opt, y_opt
+
+        return I[ind, :], y_opt_curr
+
+    def _update_maxvol(
+        self,
+        k: int,
+        forward: bool,
+        cross_strategy: CrossStrategyMaxvol,
+    ) -> None:
+        if cross_strategy.fortran_order is True:
+            combine_indices = self.combine_indices_fortran
+            order = "F"
+        else:
+            combine_indices = self.combine_indices
+            order = "C"
+        fiber = self.sample_fiber(k)
+        r_l, s, r_g = fiber.shape
+        if forward:
+            C = fiber.reshape(r_l * s, r_g, order=order)  # type: ignore
+            #Q, _ = scipy.linalg.qr(C, mode="economic", overwrite_a=True, check_finite=False)  # type: ignore
+            Q, _ = np.linalg.qr(C)
+            I, _ = self.choose_maxvol(
                 Q,  # type: ignore
                 cross_strategy.rank_kick,
                 cross_strategy.maxiter_maxvol_square,
                 cross_strategy.tol_maxvol_square,
                 cross_strategy.tol_maxvol_rect,
             )
-            cross.mps[k] = (G.T).reshape(-1, s, r_g, order=order)  # type: ignore
-            cross.I_g[k - 1] = combine_indices(cross.I_s[k], cross.I_g[k])[I]
+            if k < self.sites - 1:
+                self.I_l[k + 1] = combine_indices(self.I_l[k], self.I_s[k])[I]
         else:
-            cross.mps[0] = fiber
+            if k > 0:
+                R = fiber.reshape(r_l, s * r_g, order=order)  # type: ignore
+                #Q, _ = scipy.linalg.qr(R.T, mode="economic", overwrite_a=True, check_finite=False)
+                Q, _ = np.linalg.qr(R.T)
+                I, G = self.choose_maxvol(
+                    Q,  # type: ignore
+                    cross_strategy.rank_kick,
+                    cross_strategy.maxiter_maxvol_square,
+                    cross_strategy.tol_maxvol_square,
+                    cross_strategy.tol_maxvol_rect,
+                )
+                self.mps[k] = (G.T).reshape(-1, s, r_g, order=order)  # type: ignore
+                self.I_g[k - 1] = combine_indices(self.I_s[k], self.I_g[k])[I]
+            else:
+                self.mps[0] = fiber
 
+    def _update_maxvol_opt(
+        self,
+        k: int,
+        forward: bool,
+        cross_strategy: CrossStrategyMaxvol,
+        is_min=True
+    ) -> None:
+        if cross_strategy.fortran_order is True:
+            combine_indices = self.combine_indices_fortran
+            order = "F"
+        else:
+            combine_indices = self.combine_indices
+            order = "C"
+        fiber = self.sample_fiber(k)
+        if self.y_opt is None:
+            self.y_opt = np.inf if is_min else -np.inf
+        i_l, i_s, i_g = self.I_l[k], self.I_s[k], self.I_g[k]
+        mps_indices = self.combine_indices(i_l, i_s, i_g)
+        self.i_opt, self.y_opt = self.find_opt(mps_indices, fiber, self.i_opt, self.y_opt, is_min)
+        if is_min:
+            fiber = self.smooth_func(fiber, self.y_opt)
+        r_l, s, r_g = fiber.shape
+        if forward:
+            C = fiber.reshape(r_l * s, r_g, order=order)  # type: ignore
+            Q, _ = scipy.linalg.qr(C, mode="economic", overwrite_a=True, check_finite=False)  # type: ignore
+            I, _ = self.choose_maxvol(
+                Q,  # type: ignore
+                cross_strategy.rank_kick,
+                cross_strategy.maxiter_maxvol_square,
+                cross_strategy.tol_maxvol_square,
+                cross_strategy.tol_maxvol_rect,
+            )
+            if k < self.sites - 1:
+                self.I_l[k + 1] = combine_indices(self.I_l[k], self.I_s[k])[I]
+        else:
+            if k > 0:
+                R = fiber.reshape(r_l, s * r_g, order=order)  # type: ignore
+                Q, _ = scipy.linalg.qr(  # type: ignore
+                    R.T, mode="economic", overwrite_a=True, check_finite=False
+                )
+                I, G = self.choose_maxvol(
+                    Q,  # type: ignore
+                    cross_strategy.rank_kick,
+                    cross_strategy.maxiter_maxvol_square,
+                    cross_strategy.tol_maxvol_square,
+                    cross_strategy.tol_maxvol_rect,
+                )
+                self.mps[k] = (G.T).reshape(-1, s, r_g, order=order)  # type: ignore
+                self.I_g[k - 1] = combine_indices(self.I_s[k], self.I_g[k])[I]
+            else:
+                self.mps[0] = fiber
 
-def choose_maxvol(
-    A: np.ndarray,
-    rank_kick: tuple = (0, np.inf),
-    maxiter: int = 10,
-    tol: float = 1.1,
-    tol_rect: float = 0.1,
-) -> tuple[np.ndarray, np.ndarray]:
-    n, r = A.shape
-    max_rank_kick = min(rank_kick[1], n - r)
-    min_rank_kick = min(rank_kick[0], max_rank_kick)
-    if n < r:
-        return np.arange(n, dtype=int), np.eye(n)
-    elif rank_kick == 0:
-        return maxvol_square(A, maxiter, tol)
-    else:
-        return maxvol_rectangular(
-            A, min_rank_kick, max_rank_kick, maxiter, tol, tol_rect
+    def cross_maxvol(self,
+        cross_strategy: CrossStrategyMaxvol = CrossStrategyMaxvol(),
+        callback: Callable | None = None,
+    ) -> CrossResults:
+        """
+        Computes the MPS representation of a black-box function using the tensor cross-approximation (TCI)
+        algorithm based on one-site optimizations using the rectangular maxvol decomposition.
+        The black-box function can represent several different structures. See `black_box` for usage examples.
+
+        Parameters
+        ----------
+        black_box : BlackBox
+            The black box to approximate as a MPS.
+        cross_strategy : CrossStrategy, default=CrossStrategy()
+            A dataclass containing the parameters of the algorithm.
+        initial_points : np.ndarray, optional
+            A collection of initial points used to initialize the algorithm.
+            If None, an initial random point is used.
+        callback : Callable, optional
+            A callable called on the MPS after each iteration.
+            The output of the callback is included in a list 'callback_output' in CrossResults.
+
+        Returns
+        -------
+        CrossResults
+            A dataclass containing the MPS representation of the black-box function,
+            among other useful information.
+        """
+        converged = False
+        callback_output = []
+        with make_logger(2) as logger:
+            for i in range(self.cross_strategy.maxiter):
+                # Forward sweep
+                for k in range(self.sites):
+                    self._update_maxvol(k, True, cross_strategy)
+                # Backward sweep
+                for k in reversed(range(self.sites)):
+                    self._update_maxvol(k, False, cross_strategy)
+                if callback:
+                    callback_output.append(callback(self.mps, logger=logger))
+                if converged := _check_convergence(self, i, cross_strategy, logger):
+                    break
+            if not converged:
+                logger("Maximum number of iterations reached")
+        points = self.indices_to_points(False)
+        return CrossResults(
+            mps=self.mps,
+            points=points,
+            evals=self.black_box.evals,
+            callback_output=callback_output,
         )
 
+    def optimize(self, is_min : bool = True,
+                 max_func_evals : int = 10000,
+                 callback: Callable | None = None,
+                ) -> CrossResults:
+        """
+        Computes the MPS representation of a black-box function using the tensor cross-approximation (TCI)
+        algorithm based on one-site optimizations using the rectangular maxvol decomposition.
+        The black-box function can represent several different structures. See `black_box` for usage examples.
+
+        Parameters
+        ----------
+        black_box : BlackBox
+            The black box to approximate as a MPS.
+        cross_strategy : CrossStrategy, default=CrossStrategy()
+            A dataclass containing the parameters of the algorithm.
+        initial_points : np.ndarray, optional
+            A collection of initial points used to initialize the algorithm.
+            If None, an initial random point is used.
+        callback : Callable, optional
+            A callable called on the MPS after each iteration.
+            The output of the callback is included in a list 'callback_output' in CrossResults.
+
+        Returns
+        -------
+        CrossResults
+            A dataclass containing the MPS representation of the black-box function,
+            among other useful information.
+        """
+        converged = False
+        stop = False
+        callback_output = []
+        with make_logger(2) as logger:
+            for i in range(self.cross_strategy.maxiter):
+                # Forward sweep
+                for k in range(self.sites):
+                    self._update_maxvol_opt(k, True, self.cross_strategy, is_min)
+                if self.black_box.evals > max_func_evals:
+                    stop=True
+                    break
+                # Backward sweep
+                for k in reversed(range(self.sites)):
+                    self._update_maxvol_opt(k, False, self.cross_strategy, is_min)
+                if self.black_box.evals > max_func_evals:
+                    stop=True
+                    break
+                if callback:
+                    callback_output.append(callback(self.mps, logger=logger))
+                #if converged := _check_convergence(self, i, self.cross_strategy, logger):
+                    #break
+                if stop:
+                    logger("Maximum number of evaluations reached")
+                    break
+            if not converged:
+                logger("Maximum number of iterations reached")
+        evals = self.black_box.evals
+        points = self.indices_to_points(False)
+        dtype = [('idx', 'U50'), ('F', 'f8'), ('evals', 'i4'), ('max_D', 'i4')]
+        self.opt_trajectory = np.array(self.opt_trajectory, dtype)
+        return CrossResults(
+            mps=self.mps,
+            points=points,
+            evals=evals,
+            callback_output=callback_output,
+            opt_trajectory=self.opt_trajectory,
+            i_opt=self.i_opt,
+            y_opt=self.y_opt
+        )
 
 def maxvol_rectangular(
     A: np.ndarray,
