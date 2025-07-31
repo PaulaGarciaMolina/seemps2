5ff1856239924796aa72aba276b01b397da5fe08
PaulaGarciaMolina <paula.garciamolina@estudiante.uam.es>
2025-07-31 10:15:44 +0200

    Remove unnecessary files

--- src/seemps/evolution/__init__.py
+++
@@ -1,6 +0,0 @@
-from .arnoldi import arnoldi
-from .euler import euler
-from .runge_kutta import runge_kutta, runge_kutta_fehlberg
-from . import trotter
-
-__all__ = ["arnoldi", "euler", "runge_kutta", "runge_kutta_fehlberg", "trotter"]
--- src/seemps/register/__init__.py
+++
@@ -1,4 +0,0 @@
-from .qubo import qubo_exponential_mpo, qubo_mpo
-from .transforms import twoscomplement
-
-__all__ = ["qubo_exponential_mpo", "qubo_mpo", "twoscomplement"]
